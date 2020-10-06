255a595af89934ac6bf646e6443371fb89eb637f
Aryav <tiwarikaustav@gmail.com>
2020-10-06 21:20:28 +0530

    initial commit

---
+++ UserRegistrationValidationProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationValidationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Data Validation Program!");
+        }
+    }
+}
---
+++ UserRegistrationValidationProgram/UserData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistrationValidationProgram
+{
+    class UserData
+    {
+        public string firstName, lastName, eMail, phoneNum, password;
+
+        public void EnterFirstName()
+        {
+            Console.WriteLine("Enter First Name:");
+            string firstName = Console.ReadLine();
+            this.firstName = firstName;
+        }
+        
+        public void EnterLastName()
+        {
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            this.lastName = lastName;
+        }
+
+        public void EnterEmail()
+        {
+            Console.WriteLine("Enter email ID:");
+            string eMail = Console.ReadLine();
+            this.eMail = eMail;
+        }
+
+        public void EnterPhoneNumber()
+        {
+            Console.WriteLine("Enter Phone Number:");
+            string phoneNumber = Console.ReadLine();
+            this.phoneNum = phoneNumber;
+        }
+
+        public void EnterPassword()
+        {
+            Console.WriteLine("Enter Password:");
+            string password = Console.ReadLine();
+            this.password = password;
+        }
+    }
+}
