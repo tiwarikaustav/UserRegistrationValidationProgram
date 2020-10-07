506b58c40572c047a36456681bd9bbc79306d4aa
Aryav <tiwarikaustav@gmail.com>
2020-10-07 08:02:49 +0530

    regex expression added for email

--- UserRegistrationValidationProgram/UserDataValidation.cs
+++ UserRegistrationValidationProgram/UserDataValidation.cs
@@ -9,6 +9,9 @@ namespace UserRegistrationValidationProgram
     {
         public static string firstNameRegex = "^[A-Z]{1}[A-Za-z]{3,}$";
         public static string lastNameRegex = "^[A-Z]{1}[A-Za-z]{3,}$";
+        public static string eMailRegex = "^[a-z0-9]+[.a-z0-9]*[@][a-z]+[.][a-z]+[.a-z]*$";
+        public static string phoneNumberRegex = "^[0-9]{2}[ ][0-9]{10}$";
+        public static string passwordRegex = "^[A-Za-z0-9]{8,}$";
 
         public void IsValidFirstName(string firstName)
         {
@@ -27,5 +30,30 @@ namespace UserRegistrationValidationProgram
             }
             else Console.WriteLine("Invalid Entry!");
         }
+        public void IsValidEmail(string email)
+        {
+            if (Regex.IsMatch(email, eMailRegex))
+            {
+                Console.WriteLine("EMail Validated.");
+            }
+            else Console.WriteLine("Invalid Entry!");
+        }
+        public void IsValidPhoneNumber(string phoneNumber)
+        {
+            if (Regex.IsMatch(phoneNumber, phoneNumberRegex))
+            {
+                Console.WriteLine("Phone Number Validated.");
+            }
+            else Console.WriteLine("Invalid Entry!");
+        }
+        public void IsValidPassword(string password)
+        {
+            if (Regex.IsMatch(password, passwordRegex))
+            {
+                Console.WriteLine("Password Validated.");
+            }
+            else Console.WriteLine("Invalid Entry!");
+        }
+
     }
 }
